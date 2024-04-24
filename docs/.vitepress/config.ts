b9bdf325c7317c5d59ab05d34d24c521ba9164dd
Tobi Bueck <tobias.bueck@softoft.de>
2024-04-24 09:55:17 +0200

    Created Home Page!

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,34 @@
+export default {
+    title: 'pdgen Documentation',
+    description: 'Automatically generate UML class diagrams from Python code with pdgen.',
+    base: '/',
+
+    themeConfig: {
+        repo: 'https://github.com/yourusername/pdgen',
+        docsDir: 'docs',
+        editLinks: true,
+        editLinkText: 'Help us improve this page!',
+        lastUpdated: 'Last Updated',
+
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'GitHub', link: 'https://github.com/softoft/pdgen' }
+        ],
+
+        sidebar: {
+            '/guide/': [
+                {
+                    text: 'Guide',
+                    children: [
+                        { text: 'Introduction', link: '/guide/introduction' },
+                        { text: 'Installation', link: '/guide/installation' },
+                        { text: 'Usage', link: '/guide/usage' },
+                        { text: 'Configuration', link: '/guide/configuration' },
+                        { text: 'Advanced Features', link: '/guide/advanced' }
+                    ]
+                }
+            ],
+        }
+    }
+}
